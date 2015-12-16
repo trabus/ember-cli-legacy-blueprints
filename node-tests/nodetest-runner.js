81e29b099cb848f62d4cc42952eaf4d4faa78131
Jake Bixby <trabus@gmail.com>
2015-12-16 08:30:42 -0800

    moved blueprint-tests into node-tests/blueprints

--- node-tests/nodetest-runner.js
+++ node-tests/nodetest-runner.js
@@ -13,14 +13,14 @@ if (process.env.EOLNEWLINE) {
 rimraf.sync('.node_modules-tmp');
 rimraf.sync('.bower_components-tmp');
 
-var root = 'tests/{blueprints,acceptance,unit}';
-var _checkOnlyInTests = Promise.denodeify(mochaOnlyDetector.checkFolder.bind(null, root + '/**/*{-nodetest}.js'));
+var root = 'node-tests/{blueprints,acceptance,unit}';
+var _checkOnlyInTests = Promise.denodeify(mochaOnlyDetector.checkFolder.bind(null, root + '/**/*{-test}.js'));
 var optionOrFile = process.argv[2];
 var mocha = new Mocha({
   timeout: 5000,
   reporter: 'spec'
 });
-var testFiles = glob.sync(root + '/**/*-nodetest.js');
+var testFiles = glob.sync(root + '/**/*-test.js');
 /*var jshintPosition = testFiles.indexOf('tests/unit/jshint-test.js');
 var jshint = testFiles.splice(jshintPosition, 1);
 
@@ -28,16 +28,16 @@ testFiles = jshint.concat(testFiles);
 */
 if (optionOrFile === 'all') {
   addFiles(mocha, testFiles);
-  addFiles(mocha, '/**/*-nodetest-slow.js');
+  addFiles(mocha, 'node-tests/**/*-test.js');
+  addFiles(mocha, '/**/*-test-slow.js');
 } else if (process.argv.length > 2)  {
   addFiles(mocha, process.argv.slice(2));
 } else {
   addFiles(mocha, testFiles);
-  addFiles(mocha, 'blueprints/**/*-nodetest.js');
 }
 
 function addFiles(mocha, files) {
-  files = (typeof files === 'string') ? glob.sync(files) : files;
+  files = (typeof files === 'string') ? glob.sync(root + files) : files;
   files.forEach(mocha.addFile.bind(mocha));
 }
 
